ff4c4ebf39fb46d1203226cdf07e202db8ab9c33
LucasA75 <lucasacevedocastro@gmail.com>
2023-11-16 15:06:35 -0300

    first commit}

---
+++ main.py
@@ -0,0 +1,6 @@
+# El objetivo de este proyecto es traer informacion de una api de 
+# aliexpress de las ultimas compras y que esto se vaya a un excel 
+
+# Como añadido: que se pueda enviar un reporte mensual 
+# con las compras realizadas al correo 
+
