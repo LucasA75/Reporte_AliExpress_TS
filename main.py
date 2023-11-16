b5fa796f5748ef2cf9bc19600f8c13f47dfd4d28
LucasA75 <lucasacevedocastro@gmail.com>
2023-11-16 17:01:55 -0300

    feat: add pandas and dataframe

--- main.py
+++ main.py
@@ -4,3 +4,20 @@
 # Como añadido: que se pueda enviar un reporte mensual 
 # con las compras realizadas al correo 
 
+import pandas as pd
+
+datos = [{'Nombre':'Bulbasaur',
+         'Cantidad': 2,
+         'Precio': 4000},
+         {'Nombre': 'Charmander',
+         'Cantidad': 1,
+         'Precio': 9000}]
+
+df = pd.DataFrame(datos)
+
+
+archivo_excel = 'EjemploExportacion.xlsx'
+
+df.to_excel(archivo_excel, index=False)
+
+print(f'Se ha exportado el DataFrame a {archivo_excel}')
